92a88e53f85873eb7f29266eac76fc43511e8fba
Pawan Kumar <pawankumar9aa@gmail.com>
2025-08-13 09:16:21 +0530

    first file

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+   public static void main(String[] args) {
+    System.out.println("Pawan Kumar");
+    System.out.println("FB");
+    System.out.println(48);
+
+
+    
+   }
+  
+}
