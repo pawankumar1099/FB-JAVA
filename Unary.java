826881005f08b052a01145ee8620ed9f62144ec9
Pawan Kumar <pawankumar9aa@gmail.com>
2025-08-14 10:19:28 +0530

    add file

---
+++ Unary.java
@@ -0,0 +1,9 @@
+public class Unary {
+    public static void main(String[] args) {
+        int a = 5;
+        int b=5;
+        ++a;
+        System.out.println(a++);
+        System.out.println(++b);
+    }
+}
---
+++ bitwise.java
@@ -0,0 +1,9 @@
+public class bitwise {
+    public static void main(String[] args) {
+        System.out.println(2&3);
+        System.out.println(6|5);
+        System.out.println(6^5);
+        System.out.println(~5);
+    }
+    
+}
