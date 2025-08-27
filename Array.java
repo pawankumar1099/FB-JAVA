9c6b6bda48a814a792a5c2621508068955241c0c
Pawan Kumar <pawankumar9aa@gmail.com>
2025-08-27 08:30:30 +0530

    change folder

--- Array.java
+++
@@ -1,6 +0,0 @@
-public class Array {
-    public static void main(String[] args) {
-        int[] arr = {1, 7, 9};
-        System.out.println(arr[0]+" "+arr[1]+" "+ arr[2]);
-    }
-}
--- Unary.java
+++
@@ -1,9 +0,0 @@
-public class Unary {
-    public static void main(String[] args) {
-        int a = 5;
-        int b=5;
-        ++a;
-        System.out.println(a++);
-        System.out.println(++b);
-    }
-}
