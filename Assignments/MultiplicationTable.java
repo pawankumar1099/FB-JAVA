082255b97dde61a8216f0ab676985c95b6393d77
Pawan Kumar <pawankumar9aa@gmail.com>
2025-08-27 13:58:44 +0530

    add files

---
+++ Assignments/MultiplicationTable.java
@@ -0,0 +1,10 @@
+package Assignments;
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Digit:");
+    }
+}
--- Assignments/SumofNaturalNumbers.java
+++ Assignments/SumofNaturalNumbers.java
@@ -1,8 +1,22 @@
 package Assignments;
 
+import java.util.*;
+
 public class SumofNaturalNumbers {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Digit:");
+        int sum = 0;
+        int inp = sc.nextInt();
+
+        for(int i = 1; i <= inp ; i++){
+            sum+=i;
+        }
+
+        System.out.print("The Sum is: "+sum);
+        sc.close();
+
+
     }
     
 }
